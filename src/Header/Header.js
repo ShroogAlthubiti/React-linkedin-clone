d2945fabf8b67e776db7c2d78aa89d1dcae15172
shroog <shroog.1997s@gmail.com>
2023-10-22 18:37:45 +0300

    Final Project

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import './Header.css'
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import WorkIcon from '@mui/icons-material/Work';
-import MessageRoundedIcon from '@mui/icons-material/MessageRounded';
-import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
-import SupervisorAccountRoundedIcon from '@mui/icons-material/SupervisorAccountRounded';
-import HeaderOption from './HeaderOption';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, selectUser } from '../features/userSlice';
-import { auth } from '../Feed/firebase';
+import React from "react";
+import "./Header.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import WorkIcon from "@mui/icons-material/Work";
+import MessageRoundedIcon from "@mui/icons-material/MessageRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import SupervisorAccountRoundedIcon from "@mui/icons-material/SupervisorAccountRounded";
+import HeaderOption from "./HeaderOption";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../features/userSlice";
+import { auth } from "../Feed/firebase";
 function Header() {
- const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  const logoutOfApp = () =>{
-    dispatch(logout()) 
+  const logoutOfApp = () => {
+    dispatch(logout());
     auth.signOut();
   };
   return (
-    <div className='header'>
-        
-        <div className='header__left'>
-        <img src='https://img.icons8.com/fluency/48/linkedin.png'/>    
-        <div className='header__search'>
-        <SearchIcon/>
-        <input placeholder='Search' type='text'/>
+    <div className="header">
+      <div className="header__left">
+        <img src="https://img.icons8.com/fluency/48/linkedin.png" />
+        <div className="header__search">
+          <SearchIcon />
+          <input placeholder="Search" type="text" />
         </div>
-        </div>
-        <div className='header__right'>
-
-            <HeaderOption title='Home' Icon={HomeIcon}/>
-            <HeaderOption title='My Notwork' Icon={SupervisorAccountRoundedIcon}/>
-            <HeaderOption title='Jobs' Icon={WorkIcon}/>
-            <HeaderOption title='Messaging' Icon={MessageRoundedIcon}/>
-            <HeaderOption title='Notifications' Icon={NotificationsRoundedIcon}/>
-            <HeaderOption   avater={user?.photo } title='logout'onClick={logoutOfApp}/>
-        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My Notwork" Icon={SupervisorAccountRoundedIcon} />
+        <HeaderOption title="Jobs" Icon={WorkIcon} />
+        <HeaderOption title="Messaging" Icon={MessageRoundedIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationsRoundedIcon} />
+        <HeaderOption
+          avater={user?.photo}
+          title="logout"
+          onClick={logoutOfApp}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -1,13 +1,30 @@
 import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
-
+import "./Widgets.css";
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 function Widgets() {
+  const newArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+       <FiberManualRecordIcon/>
+      </div>
+      <div className="widgets__articleRight">
+        <h2>{heading}</h2>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
   return (
     <div className="widgets">
       <div className="widgets__header">
         <h2>LinkedIn News</h2>
         <InfoIcon />
       </div>
+      {newArticle("Shroog is the best!!", "Top news - 555 readers")}
+      {newArticle("PHP ", "Top news - 555 readers")}
+      {newArticle("Shroog is the best!!", "Top news - 555 readers")}
+      {newArticle("Shroog is the best!!", "Top news - 555 readers")}
+      {newArticle("Shroog is the best!!", "Top news - 555 readers")}
     </div>
   );
 }
